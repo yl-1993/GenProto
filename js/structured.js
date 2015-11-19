06883ee15c27e2ca8edf75ad28718dec12cc42da
yl-1993 <yl93528@gmail.com>
2015-11-19 17:38:08 +0800

    show unit setting

--- js/structured.js
+++ js/structured.js
@@ -832,6 +832,65 @@ layer {
 
 */});
 
+var CONV_BN_LAYER_TEMPLATE = doc2str(function(){/*
+layer {
+  name: "conv#%#NAME#%#"
+  type: "ConvolutionData"
+  bottom: "#%#BOTTOM#%#"
+  top: "conv#%#NAME#%#"
+  convolution_param {
+    num_output: #%#CHANNELS#%#
+    pad: 1
+    kernel_size: 3
+    stride: 1
+    weight_filler {
+      type: "msra"
+      variance_norm: FAN_OUT
+    }
+    bias_filler {
+      type: "constant"
+      value: 0.0
+    }
+  }
+}
+layer {
+  name : "conv#%#NAME#%#_bn"
+  type : "BNData"
+  bottom : "conv#%#NAME#%#"
+  top : "conv#%#NAME#%#_bn"
+  param {
+    lr_mult : 1
+  }
+  param {
+    lr_mult : 1
+  }
+  param {
+    lr_mult : 0
+  }
+  param {
+    lr_mult : 0
+  }
+  bn_param {
+    scale_filler {
+      type : "constant"
+      value : 1
+    }
+    shift_filler {
+      type : "constant"
+    }
+    moving_average : true
+    decay : 0.05
+  }
+}
+layer {
+  name: "prelu#%#NAME#%#"
+  type: "PReLU"
+  bottom: "conv#%#NAME#%#_bn"
+  top: "prelu#%#NAME#%#"
+}
+
+*/});
+
 var STAGE_SPLIT_TEMPLATE = doc2str(function(){/*
 layer {
   name: "pool#%#NAME#%#"
@@ -858,23 +917,6 @@ layer {
     source: "train.txt"
     batch_size : 256
     is_color : 1
-  }
-  include {
-    phase: TRAIN  
-  }
-}
-layer {
-  name : "image_input"
-  type : "ImageData"
-  top : "data"
-  top : "label"
-  image_data_param {
-    source: "test.txt"
-    batch_size : 64
-    is_color: 1
-  }
-  include {
-    phase: TEST  
   }
 }
 
@@ -917,7 +959,7 @@ layer {
   bottom : "output"
   top: "output"
   dropout_param {
-    dropout_ratio: 0.2
+    dropout_ratio: 0.5
   }
 }
 layer {
@@ -931,12 +973,29 @@ layer {
 '*/});
 
 function changeSNPanel() {
+  'use strict'
   var _type = document.getElementById('sn_unit_type').value;
+  if (_type.search('conv')>=0) {
+    document.getElementById('sn_pool_method').value = "MAX";
+  } 
   if (_type.search('inception')>=0) {
     document.getElementById('sn_tr_inception_type').style.display = "block";
+    document.getElementById('sn_pool_method').value = "AVE";
   } else {
     document.getElementById('sn_tr_inception_type').style.display = "none";
   }
+
+  var _isbn = document.getElementById('sn_bn').value;
+  if (_isbn == "1") _type += "_bn";
+  var _pool_method = document.getElementById('sn_pool_method').value; 
+  var _inception_method = document.getElementById('sn_inception_type').value;
+  var _unit_setting = "";
+  var _stage = {0:[16]};
+  var _unit_setting = gen_structured_unit(_stage, _type, _isbn, _pool_method, _inception_method);
+
+  document.getElementById("prototxt").value = DATA_LAYER_TEMPLATE+_unit_setting;
+  editor_proto.setValue(document.getElementById("prototxt").value);
+  gen_model();
 }
 
 
@@ -945,6 +1004,12 @@ function get_common_template(type) {
     case 'conv':
       return {
         "template": CONV_LAYER_TEMPLATE,
+        "name": "conv",
+        "output": "prelu"
+      };
+    case 'conv_bn':
+      return {
+        "template": CONV_BN_LAYER_TEMPLATE,
         "name": "conv",
         "output": "prelu"
       };
@@ -971,59 +1036,19 @@ function get_common_template(type) {
   }
 }
 
-function gen_structured_network() {
-	var _network = '';
-	var _data_layer = '';
-	var _common_layer = '';
-	var _loss_layer = '';
-  var _type = ''
-  var _isbn = '';
-  var _stage = {};
-  var _idx, _count, _name, _num_output, _length;
-  var _bottom, _top, _channel;
-  var _channel01, _channel02, _channel03, _channel04;
+function gen_structured_unit(_stage, _type, _isbn, _pool_method, _inception_method) {
   var i, j;
-
-  var _stages_from_ui = document.getElementById('stages').value;
-  var _filters_from_ui = document.getElementById('filters').value;
-
-  if (isNaN(_stages_from_ui) || !_stages_from_ui) {
-    showErrorToast('Please fill the stages with posititve number!');
-    return;
-  }
-
-  var _filters_each_stage = _filters_from_ui.split(';');
-
-  for (i=0; i < _stages_from_ui; ++i) {
-    _stage[i] = _filters_each_stage[i].split(",");
-    for (j=0; j < _stage[i].length; ++j) {
-      _stage[i][j] = parseInt(_stage[i][j]);
-    }
-  }
-
-  _type = document.getElementById('sn_unit_type').value;
-  //_type = 'inception_bn';
-  //_type = 'inception';
-  //_type = 'inception_mb';
-  //_type = 'conv'
-  _isbn = document.getElementById('sn_bn').value;
-  if (_isbn == "1") _type += "_bn";
-
-  _pool_method = document.getElementById('sn_pool_method').value;
-  //_pool_method = 'MAX'
-  //_pool_method = 'AVE'
-
-  _inception_method = document.getElementById('sn_inception_type').value;
-  //_inception_method = 'up'
-  //_inception_method = 'down'
-  //_inception_method = 'equal'
-
+  var _tmp_str;
+  var _bottom, _top, _channel;
   var _stat = get_common_template(_type);
+  var _idx, _count, _name, _num_output, _length;
+  var _channel01, _channel02, _channel03, _channel04;
+  
   var _common_template = _stat["template"];
   var _name_pattern = _stat["name"];
   var _output_pattern = _stat["output"];
 
-	_data_layer = DATA_LAYER_TEMPLATE;
+  var _common_layer = '';
 
   _bottom = "data"
   for(_idx in _stage) {
@@ -1033,7 +1058,7 @@ function gen_structured_network() {
       _channel = _stage[_idx][_count-1];
       _tmp_str = _common_template.replace(/#%#BOTTOM#%#/g, _bottom);
       _tmp_str = _tmp_str.replace(/#%#NAME#%#/g, _name);
-      if (_type == 'conv') {
+      if (_type == 'conv' || _type == 'conv_bn') {
         _tmp_str = _tmp_str.replace(/#%#CHANNELS#%#/g, _channel);
       } else if (_type == 'inception_bn' || _type == 'inception' || _type == 'inception_mb') {
         if (_inception_method == 'up') {
@@ -1067,10 +1092,48 @@ function gen_structured_network() {
     _bottom = "pool"+_name;
     _common_layer += _tmp_str.replace(/#%#NAME#%#/g, _name)
   }
+  return _common_layer;
+}
 
+function gen_structured_network() {
+  'use strict'
+  var i;
+	var _network = '';
+	var _data_layer = '';
+  var _common_layer = '';
+	var _loss_layer = '';
+  var _type = ''
+  var _isbn = '';
+  var _stage = {};
+
+  var _stages_from_ui = document.getElementById('stages').value;
+  var _filters_from_ui = document.getElementById('filters').value;
+
+  if (isNaN(_stages_from_ui) || !_stages_from_ui) {
+    showErrorToast('Please fill the stages with posititve number!');
+    return;
+  }
+
+  var _filters_each_stage = _filters_from_ui.split(';');
+
+  for (i=0; i < _stages_from_ui; ++i) {
+    _stage[i] = _filters_each_stage[i].split(",");
+    for (j=0; j < _stage[i].length; ++j) {
+      _stage[i][j] = parseInt(_stage[i][j]);
+    }
+  }
+
+  _type = document.getElementById('sn_unit_type').value;
+  _isbn = document.getElementById('sn_bn').value;
+  if (_isbn == "1") _type += "_bn";
+  var _pool_method = document.getElementById('sn_pool_method').value; 
+  var _inception_method = document.getElementById('sn_inception_type').value;
+
+  _data_layer = DATA_LAYER_TEMPLATE;
+  _common_layer = gen_structured_unit(_stage, _type, _isbn, _pool_method, _inception_method);
   _loss_layer = LOSS_LAYER_TEMPLATE.replace("#%#BOTTOM#%#", _bottom);
   _network = _data_layer+_common_layer+_loss_layer;
 
 	document.getElementById("prototxt").value = _network;
-    editor_proto.setValue(document.getElementById("prototxt").value);
+  editor_proto.setValue(document.getElementById("prototxt").value);
 }
